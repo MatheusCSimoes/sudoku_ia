5e2c6088fb8a3fff465feb220ae726548296cd79
MatheusCSimoes <48828361+MatheusCSimoes@users.noreply.github.com>
2019-12-08 18:18:13 -0300

    Add files via upload

--- js/genetico.js
+++ js/genetico.js
@@ -1,22 +1,8 @@
-// var populationSize = 2000;
-// var elitism = true;
-// var crossProb = 0.9;
-// var mutationProb = 0.05;
-// var maxNumGenerations = 500;
-
-// chegou em 9
-// var populationSize = 2000;
-// var elitism = true;
-// var crossProb = 0.9;
-// var mutationProb = 0.15;
-// var maxNumGenerations = 600;
-var testsCount = 0;
-
-var populationSize = 10;
+var populationSize = 1000;
 var elitism = true;
 var crossProb = 0.8;
-var mutationProb = 0.05;
-var maxNumGenerations = 50;
+var mutationProb = 0.1;
+var maxNumGenerations = 1000;
 
 var generation = 0;
 var population = []
@@ -29,46 +15,59 @@ var missing = [];
 var sudokuMatrix = [];
 var amount = [0,0,0,0,0,0,0,0,0,0];
 var intermediatePopulation = [];
+var elite = [];
 
 var historyTests = [];
 
+var cellsFixed = false;
+
 const genetico = () => {
-  testsCount += 1;
+  if(!cellsFixed) {
+    fixCells();
+    cellsFixed = true;
+  }
+
   objectiveFunc = 0;
 	countNumbers() //conta quantos numeros ja estao no tabuleiro
 	createMissing() //cria array com numeros missing
 
 	initData()
 
-	// console.log("Começo:",clone(populationObjectiveFuncSum))
-  // console.log("População:")
-	// console.table(clone(population))
-
-  // console.log("New genetic population test:")
-  // console.log("elistim:",elitism)
-  // console.log("populationSize:",populationSize)
-  // console.log("crossProb:",crossProb)
-  // console.log("mutationProb:",mutationProb)
-  // console.log("maxNumGenerations:",maxNumGenerations)
-
   initializeMatrix();
   let index = getBestIndividualIndex();
-  updateSudoku(clone(population[index].individual), false);
+  updateSudoku(cloneObject(population[index].individual), false);
 
   nextGeneration()
 
   while(generation < maxNumGenerations) {
-    nextGeneration();
-    console.log("População:")
-  	console.table(clone(population))
+    if(population[index].objectiveFunc <= 0) {
+      console.log("Solution found!!!");
+      initializeMatrix();
+      let solution = getBestIndividualIndex();
+      updateSudoku(cloneObject(population[solution].individual), true);
+      historyTests.push({
+        elitism: elitism,
+        populationSize: populationSize,
+        crossProb: crossProb,
+        mutationProb: mutationProb,
+        maxNumGenerations: maxNumGenerations,
+        foundSolution: true,
+        resultAdaptFunc: cloneObject(population[index].objectiveFunc),
+        bestIndividual: cloneObject(population[index].individual)
+      });
+
+      break;
+    }
+    else {
+      nextGeneration();
+    }
   }
 
   initializeMatrix();
   index = getBestIndividualIndex();
-  // updateSudoku(clone(population[index].individual), true);
-  updateSudoku(clone(population[index].individual), false);
+  updateSudoku(cloneObject(population[index].individual), true);
 
-  console.log("TestsCount = ", testsCount, " AdaptFunc -> ", clone(population[index].objectiveFunc));
+  console.log(" AdaptFunc -> ", cloneObject(population[index].objectiveFunc));
 
   historyTests.push({
     elitism: elitism,
@@ -77,8 +76,8 @@ const genetico = () => {
     mutationProb: mutationProb,
     maxNumGenerations: maxNumGenerations,
     foundSolution: false,
-    resultAdaptFunc: clone(population[index].objectiveFunc),
-    bestIndividual: clone(population[index].individual)
+    resultAdaptFunc: cloneObject(population[index].objectiveFunc),
+    bestIndividual: cloneObject(population[index].individual)
   });
 }
 
@@ -86,6 +85,7 @@ const nextGeneration = () => {
   selection();
   crossover();
   mutation();
+  addElite();
 
   populationObjectiveFuncForMax = 0
   populationObjectiveFuncSum = 0
@@ -93,75 +93,30 @@ const nextGeneration = () => {
   //recalculate objective function
   for(let i = 0; i < populationSize; i++) {
     initializeMatrix();
-    individual = clone(population[i].individual);
-    updateSudoku(clone(population[i].individual), false);
+    individual = cloneObject(population[i].individual);
+    updateSudoku(cloneObject(population[i].individual), false);
 
     objectiveFunc = 0;
     objectiveFunc = calcObjectiveFunc();
-    population[i].objectiveFunc = clone(objectiveFunc);
-    population[i].objectiveFuncForMax = 1 / (clone(objectiveFunc) + 1);
+    population[i].objectiveFunc = cloneObject(objectiveFunc);
+    population[i].objectiveFuncForMax = 1 / (cloneObject(objectiveFunc) + 1);
 
-    populationObjectiveFuncForMax += (1 / (clone(objectiveFunc) + 1))
-    populationObjectiveFuncSum += clone(objectiveFunc)
+    populationObjectiveFuncForMax += (1 / (cloneObject(objectiveFunc) + 1))
+    populationObjectiveFuncSum += cloneObject(objectiveFunc)
   }
 
   generation = generation + 1;
-  console.log("Geração:",clone(generation))
-  // console.log("Começo:",clone(populationObjectiveFuncSum))
-
-  // console.log("População:")
-  // console.table(clone(population));
+  console.log("Geração:",cloneObject(generation))
+  console.log("Population sum:",cloneObject(populationObjectiveFuncSum))
 
   initializeMatrix();
   let index = getBestIndividualIndex();
-  updateSudoku(clone(population[index].individual), false);
-  // console.log("bestIndividual: ",clone(population[index].individual))
+  updateSudoku(cloneObject(population[index].individual), false);
 
-  console.log("bestIndividualObjectiveFunc: ",clone(population[index].objectiveFunc));
+  console.log("bestIndividualObjectiveFunc: ",cloneObject(population[index].objectiveFunc));
 
-  if(population[index].objectiveFunc <= 0) {
-    console.log("População:")
-    console.table(clone(population));
-
-    console.log("Solution found!!!");
-    updateSudoku(clone(population[index].individual), true);
-    // console.log("Solution individual:",clone(population[index].individual));
-    historyTests.push({
-      elitism: elitism,
-      populationSize: populationSize,
-      crossProb: crossProb,
-      mutationProb: mutationProb,
-      maxNumGenerations: maxNumGenerations,
-      foundSolution: true,
-      resultAdaptFunc: clone(population[index].objectiveFunc),
-      bestIndividual: clone(population[index].individual)
-    });
-    return;
-  }
-
-  // if(generation <= maxNumGenerations) {
-  //   // nextGeneration();
-  // }
-  // else {
-  //   // console.log("Result best individual:",clone(population[index].individual))
-  //   // console.log("Result => adaptFunc:",clone(population[index].objectiveFunc));
-  //   // console.log("População:")
-  //   // console.log(clone(population));
-  //
-  //   initializeMatrix();
-  //   updateSudoku(clone(population[index].individual), true);
-  //
-  //   historyTests.push({
-  //     elitism: elitism,
-  //     populationSize: populationSize,
-  //     crossProb: crossProb,
-  //     mutationProb: mutationProb,
-  //     maxNumGenerations: maxNumGenerations,
-  //     foundSolution: false,
-  //     resultAdaptFunc: clone(population[index].objectiveFunc),
-  //     bestIndividual: clone(population[index].individual)
-  //   });
-  // }
+  // console.log("Population:");
+  // console.table(cloneObject(population));
 }
 
 const selection = () => {
@@ -171,9 +126,8 @@ const selection = () => {
 
   if(elitism) {
     let index = getBestIndividualIndex();
-    // console.log("On selection = ", index);
-    // console.table(clone(population));
-    intermediatePopulation.push(clone(population[index]));
+    // intermediatePopulation.push(cloneObject(population[index]));
+    elite.push(cloneObject(population[index]));
   }
 
   let considerElitism = elitism ? 1 : 0;
@@ -182,100 +136,81 @@ const selection = () => {
     let prob = Math.random();
     let sumProbs = 0;
     for(let j = 0; j < populationSize; j++) {
-      sumProbs += clone(population[j].objectiveFuncForMax) / clone(populationObjectiveFuncForMax);
+      sumProbs += cloneObject(population[j].objectiveFuncForMax) / cloneObject(populationObjectiveFuncForMax);
       if(sumProbs >= prob) {
-        intermediatePopulation.push(clone(population[j]));
+        intermediatePopulation.push(cloneObject(population[j]));
         break;
       }
       if(j == populationSize -1) {
-        // console.log(sumProbs);
-        intermediatePopulation.push(clone(population[j]));
+        intermediatePopulation.push(cloneObject(population[j]));
       }
     }
   }
 
-  if(intermediatePopulation.length < populationSize) {
+  if(intermediatePopulation.length + considerElitism < populationSize) {
     throw new Error("Popolation size error, less individuals");
   }
 
-  if(intermediatePopulation.length != populationSize) {
+  if(intermediatePopulation.length + considerElitism != populationSize) {
     throw new Error("Popolation size error");
   }
-
-  // console.log("After selection:")
-  // console.table(clone(intermediatePopulation));
 }
 
 const crossover = () => {
   population = []
 
-  // let test = true;
-
-  // for(let i = 0; i < populationSize / 2; i++) {
   while(intermediatePopulation.length > 1) {
     let pos1 = Math.floor((Math.random() * intermediatePopulation.length));
     let pos2 = Math.floor((Math.random() * intermediatePopulation.length));
     while(pos1 == pos2 && intermediatePopulation.length >= 2) { pos2 = Math.floor((Math.random() * intermediatePopulation.length)); }
 
-    // if(test) console.log(pos1, " - pos -  " , pos2);
-
     if(Math.random() < crossProb) {
       let posCross = Math.floor((Math.random() * intermediatePopulation[0].individual.length));
-
-      // if(test) console.log("posCross ", posCross);
-      // test = false;
-
-      // console.log("trying cross")
-      // console.log(intermediatePopulation[pos1].individual)
-      // console.log(intermediatePopulation[pos2].individual)
 
       let removed1 = intermediatePopulation[pos1].individual.splice(posCross, 64);
       let removed2 = intermediatePopulation[pos2].individual.splice(posCross, 64);
 
       intermediatePopulation[pos1].individual = intermediatePopulation[pos1].individual.concat(removed2);
       intermediatePopulation[pos2].individual = intermediatePopulation[pos2].individual.concat(removed1);
-
-      // console.log(intermediatePopulation[pos1].individual)
-      // console.log(intermediatePopulation[pos2].individual)
     }
 
     //just to know that the individual is new but its objective function isn't calculated
     intermediatePopulation[pos1].objectiveFunc = -1;
     intermediatePopulation[pos2].objectiveFunc = -1;
 
-    population.push(clone(intermediatePopulation[pos1]));
-    population.push(clone(intermediatePopulation[pos2]));
+    population.push(cloneObject(intermediatePopulation[pos1]));
+    population.push(cloneObject(intermediatePopulation[pos2]));
 
     intermediatePopulation.splice(pos1, 1);
     pos1 > pos2 ? intermediatePopulation.splice(pos2, 1) : intermediatePopulation.splice(pos2 - 1, 1);
    }
 
-  if(populationSize % 2) {
-    population.push(clone(intermediatePopulation[0]));
+  if((populationSize - elite.length) % 2) {
+    population.push(cloneObject(intermediatePopulation[0]));
   }
 
-  if(population.length != populationSize) {
+  if(population.length + elite.length != populationSize) {
     throw new Error("Popolation size error");
   }
-
-  // console.log("After crossover")
-  // console.table(clone(population))
 }
 
 const mutation = () => {
-  for(let i = 0; i < populationSize; i++) {
+  for(let i = 0; i < population.length; i++) {
     if(Math.random() < mutationProb) {
       let individualSize = population[i].individual.length;
       let pos = Math.floor((Math.random() * individualSize));
       let change = Math.floor((Math.random() * (individualSize - pos)))
 
-      //console.log("mutation on individual ", i);
       population[i].individual[pos] = change;
     }
   }
+}
 
-  // console.log("After mutation");
-  // console.table(clone(population));
+const addElite = () => {
+  while(elite.length > 0) {
+    population.push(cloneObject(elite[0]));
+    elite.splice(0,1);
+  }
 }
 
 const initData = () => {
@@ -289,13 +224,12 @@ const initData = () => {
     initializeMatrix()
     calcIndividual()
     objectiveFunc = calcObjectiveFunc()
-    // preencherSudoku() //just if this individual is the best on his generation
-    populationObjectiveFuncForMax += (1 / (clone(objectiveFunc) + 1))
-    populationObjectiveFuncSum += clone(objectiveFunc)
+    populationObjectiveFuncForMax += (1 / (cloneObject(objectiveFunc) + 1))
+    populationObjectiveFuncSum += cloneObject(objectiveFunc)
     population.push({
-      individual: clone(individual),
-      objectiveFunc: clone(objectiveFunc),
-      objectiveFuncForMax: 1 / (clone(objectiveFunc) + 1)
+      individual: cloneObject(individual),
+      objectiveFunc: cloneObject(objectiveFunc),
+      objectiveFuncForMax: 1 / (cloneObject(objectiveFunc) + 1)
     })
   }
 }
@@ -303,12 +237,11 @@ const initData = () => {
 const countNumbers = () => {
 	for(let linha = 1; linha < 10; linha++) {
 		for(let coluna = 1; coluna < 10; coluna++) {
-			if(sudoku(linha,coluna) != 0){
-				amount[sudoku(linha,coluna)]++;
+			if(sudokuCell(linha,coluna) != 0 && document.getElementById('cell-'+(linha)+(coluna)).disabled == true){
+				amount[sudokuCell(linha,coluna)]++;
 			}
     }
   }
-  //console.log("numeros:",amount);
 }
 
 const createMissing = () => {
@@ -322,12 +255,10 @@ const createMissing = () => {
 			}
     }
   }
-	//console.log("missing:")
-	//console.log(missing)
 }
 
 const calcIndividual = () => {
-	let falt = clone(missing);
+	let falt = cloneObject(missing);
 	for(let linha = 1; linha < 10; linha += 1) {
 		for(let coluna = 1; coluna < 10; coluna += 1) {
 			if(sudokuMatrix[linha][coluna] == 0){
@@ -402,7 +333,7 @@ const initializeMatrix = () => {
 		sudokuMatrix[linha] = [];
 		for(let coluna = 1; coluna < 10; coluna += 1) {
 			if(document.getElementById('cell-'+(linha)+(coluna)).disabled == true)
-				sudokuMatrix[linha][coluna] = clone(sudokuCell(linha,coluna));
+				sudokuMatrix[linha][coluna] = cloneObject(sudokuCell(linha,coluna));
 			else
 				sudokuMatrix[linha][coluna] = 0;
     }
@@ -410,7 +341,7 @@ const initializeMatrix = () => {
 }
 
 const updateSudoku = (bestIndividual, show) => {
-	let falt = clone(missing);
+	let falt = cloneObject(missing);
 	let c = 0
 	for(let linha = 1; linha < 10; linha += 1) {
 		for(let coluna = 1; coluna < 10; coluna += 1) {
@@ -454,4 +385,53 @@ const sortPopulation = () => {
   population.sort(function(a, b) {
     return a.objectiveFunc - b.objectiveFunc;
   })
+}
+
+const fixCells = () => {
+	for(let linha = 1; linha < 10; linha += 1) {
+		for(let coluna = 1; coluna < 10; coluna += 1) {
+			if(document.getElementById('cell-'+(linha)+(coluna)).value != 0){
+				disableSudokuCell(linha,coluna);
+			}
+		}
+	}
+}
+
+const disableSudokuCell = (l,c) =>{
+	document.getElementById('cell-'+(l)+(c)).style.color = "blue";
+	document.getElementById('cell-'+(l)+(c)).disabled = true;
+}
+
+function cloneObject(obj) {
+  var copy;
+
+  // Handle the 3 simple types, and null or undefined
+  if (null == obj || "object" != typeof obj) return obj;
+
+  // Handle Date
+  if (obj instanceof Date) {
+    copy = new Date();
+    copy.setTime(obj.getTime());
+    return copy;
+  }
+
+  // Handle Array
+  if (obj instanceof Array) {
+    copy = [];
+    for (var i = 0, len = obj.length; i < len; i++) {
+      copy[i] = cloneObject(obj[i]);
+    }
+    return copy;
+  }
+
+  // Handle Object
+  if (obj instanceof Object) {
+    copy = {};
+    for (var attr in obj) {
+      if (obj.hasOwnProperty(attr)) copy[attr] = cloneObject(obj[attr]);
+    }
+    return copy;
+  }
+
+  throw new Error("Unable to copy obj! Its type isn't supported.");
 }
